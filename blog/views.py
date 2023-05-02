db09a3329fed8337256bba5d7f9348e7eaf63a36
BOGGOD <94290821+BOGGOD@users.noreply.github.com>
2023-05-02 16:35:06 +0300

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -6,8 +6,3 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
-
-
-def post_list(request):
-    return render(request, 'blog/post_list.html', {})
-
